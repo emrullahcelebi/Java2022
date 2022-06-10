362e4b6ff586c37ecdfd90034247c7a03b5d3177
Emrullah Celebi <cumali20182019@gmail.com>
2022-06-10 11:52:20 +0200

    day21

---
+++ src/day21/C01.java
@@ -0,0 +1,12 @@
+package day21;
+
+public interface C01 {
+
+    public void motor();//interface icinde abstrac methodlar ve ozel concrete methodlar olabilir
+    //beni concert eden her clas in dinamik motor ozelligi olmalidir
+
+    void vites();
+    abstract void kasa();
+
+
+}

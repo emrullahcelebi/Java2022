90c4c647fa746f0429b89f2a2b3ac626efccc351
Emrullah Celebi <cumali20182019@gmail.com>
2022-07-06 10:38:27 +0200

    day23

--- src/day24/C01.java
+++ src/day24/C01.java
@@ -1,2 +1,40 @@
-package day24;public class C01 {
+package day24;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class C01 {
+    public static void main(String[] args) {
+        Set<String> set1=new HashSet<>();
+        set1.add("B");
+        set1.add("A");
+        set1.add("B");
+        set1.add("C");
+        set1.add("B");
+        set1.add("A");
+
+        System.out.println(set1);
+
+        set1.clear();
+        System.out.println(set1);
+
+        set1.add("F");
+        set1.add("A");
+        set1.add("B");
+        set1.add("A");
+
+        Set<String> set2=new HashSet<>();
+        set2.add("B");
+        set2.add("A");
+        set2.add("F");
+        System.out.println(set1.containsAll(set2));
+
+        System.out.println(set1.equals(set2));
+
+        set1.retainAll(set2);
+        System.out.println(set1);
+        System.out.println(set2);
+
+
+    }
 }
--- src/day24/C02.java
+++ src/day24/C02.java
@@ -1,2 +1,18 @@
-package day24;public class C02 {
+package day24;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class C02 {
+    public static void main(String[] args) {
+        Set<String> set1=new LinkedHashSet<>();
+        set1.add("A");
+        set1.add("F");
+        set1.add("B");
+        System.out.println(set1);
+
+        TreeSet<String> set3=new TreeSet<>(set1);
+
+    }
 }

176711209694e7ad1ff49cc7f9e6d745cdc93371
Emrullah Celebi <cumali20182019@gmail.com>
2022-07-15 17:19:16 +0200

    day26

---
+++ src/day26/C01.java
@@ -0,0 +1,14 @@
+package day26;
+
+import java.util.HashMap;
+
+public class C01 {
+    public static void main(String[] args) {
+        HashMap<Integer,String> objeMap=new HashMap<>();
+
+        objeMap.put(101,"Ali, Can, Java");
+        objeMap.put(102,"Veli, Yan, Java");
+        objeMap.put(103,"Ali, Yan, C#");
+        System.out.println(objeMap);
+    }
+}

20b69418580d087cfe917e620f5af3f2b93b6b71
Emrullah Celebi <cumali20182019@gmail.com>
2022-07-15 08:31:45 +0200

    day25

--- src/day25/C02.java
+++ src/day25/C02.java
@@ -1,4 +1,21 @@
 package day25;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class C02 {
+    public static void main(String[] args) {
+        Deque<String> dk1=new LinkedList<>();
+        dk1.addFirst("C");
+        dk1.add("M");
+        dk1.add("A");
+        System.out.println(dk1);
+        dk1.addLast("L");
+        System.out.println(dk1);
+        System.out.println(dk1.getFirst());//C
+        System.out.println(dk1.getLast());//L
+
+        System.out.println(dk1.pop());
+        System.out.println(dk1);
+    }
 }
